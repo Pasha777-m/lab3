1b8652eb3381e8341d0c36132d375bf572518e4b
Pasha777-m <58082562+Pasha777-m@users.noreply.github.com>
2019-11-29 20:35:36 +0200

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function openContent(numberItem) {
     var i, item, item0;
     item = document.getElementsByClassName("item");
+    for (i = 0; i < item.length; i++) {
+        item[i].style.display = "none";
+    }
 }
